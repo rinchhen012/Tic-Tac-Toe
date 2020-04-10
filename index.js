0b9f34b60a452d4c7ebd76945ba0ad6ea5b5a455
rinchhen012 <rinchhen.meh@gmail.com>
2020-04-10 21:56:46 +0900

    Finished linking click from DOM to array

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 'use strict';
 
+// changes player turn starting from player X
+let xTurn = true;
 
 const boxes = document.querySelectorAll(".box");
 boxes.forEach((box) => {
     box.addEventListener('click', () => {
         let playerChoice = box.dataset.number;
-        Render();
+        Render(box, playerChoice);
     });
 });
 
-let playerSelect1 = document.getElementById('player-select1');
-let playerSelect2 = document.getElementById('player-select2');
-let p1 = playerSelect1.options[playerSelect1.selectedIndex].value;
-
+// Gameboard module is the powerhouse the game
 const Gameboard = (function () {
     const winPattern =
         [[0, 1, 2], [0, 3, 6], [1, 4, 7],
         [2, 5, 8], [3, 4, 5], [6, 7, 8],
         [0, 4, 8], [2, 4, 6]];
-    let gameBoard = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
+    let gameBoard = [];
+    let playerSelect1 = document.getElementById('player-select1');
+    let playerSelect2 = document.getElementById('player-select2');
+    let p1 = playerSelect1.innerHTML;
+    let p2 = playerSelect2.innerHTML;
+
     return {
         gameBoard,
-        winPattern
+        winPattern,
+        p1,
+        p2
     };
 })();
 
@@ -33,11 +39,21 @@ const DiplayController = (function () {
 
 })();
 
-const Render = function () {
-    if () {
-        box.innerHtml = 'X';
+const Render = function (box, playerChoice) {
+    // prevents redundancy in array
+    if (!(Gameboard.gameBoard.includes(playerChoice))) {
+        Gameboard.gameBoard.push(playerChoice);
+        if (xTurn) {
+            box.innerHTML = 'X';
+            xTurn = false;
+        }
+        else {
+            box.innerHTML = 'O';
+            xTurn = true;
+        }
     }
+
 };
 
 
-Render();
+
